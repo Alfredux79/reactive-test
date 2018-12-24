7d8e9c328dd26a67a3ffb472e5a3276df5f2b4c7
Alfredo <alfredo.martinm@gmail.com>
2018-12-24 18:39:27 +0100

    created endpoint to clear database

--- src/main/java/io/alfredux/reactivetest/controller/UtilController.java
+++ src/main/java/io/alfredux/reactivetest/controller/UtilController.java
@@ -2,7 +2,9 @@ package io.alfredux.reactivetest.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.alfredux.reactivetest.repository.SyncTransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +15,17 @@ public class UtilController {
     @Autowired
     ObjectMapper objectMapper;
 
+    @Autowired
+    SyncTransactionRepository transactionRepository;
+
     @PostMapping("echo")
     public Object echo(@RequestBody Object body) throws JsonProcessingException {
         System.out.println(objectMapper.writeValueAsString(body));
         return body;
+    }
+
+    @GetMapping("clear")
+    public void clearRepository() {
+        transactionRepository.deleteAll();
     }
 }
